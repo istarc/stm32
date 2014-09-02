f853d3ee971a2dd33106bdeb9863e44884c4d491
Iztok Starc <iztok.starc@gmail.com>
2014-09-02 14:25:05 +0200

    Updated build script.

--- build-ARM-toolchain/build.sh
+++ build-ARM-toolchain/build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+
 # GNU ARM Cross-Toolchain Builder
 
 ###
 # Builds GNU ARM Cross-Toolchain from Scratch
-#  Host:   x86_64
-#  Target: cortex-m4f
+#
+#  Host:   Ubuntu 14.04 LTS x86_64
+#  Target: ARM cortex-m4f
+#
+# Features:
+# - GNU C (4.9.1)
+# -  with newlib (2.1.0)
+# - GNU C++ (4.9.1, implements: C++14, C++11)
+# -  with libstdc++ (4.9.1)
+# - GDB (7.8)
 
 ###
 # Prerequisites:
@@ -32,14 +41,14 @@
 # Test the Toolchain
 # - Connect STM32F4 Discovery Board
 # - Open a new console and type
-#    export PATH=$PATH:$PREFIX/bin
+#    export PATH=$PREFIX/bin:$PATH
 #    cd stm32/examples/OOP
 #    make clean && make all
 #    make deploy
 
 ###
-# Check My Blog ;-)
-#  istarc.wordpress.com
+# Check My Blog
+#  http://istarc.wordpress.com
 
 ###
 # 1. Define Variables
@@ -47,39 +56,75 @@
 export BINUTILS=binutils-2.24
 export GCC=gcc-4.9.1
 export NEWLIB=newlib-2.1.0
-export GDB=gdb-7.7
+export GDB=gdb-7.8
 # 1.2 Target system
 export TARGET=arm-none-eabi
 # 1.3 Build directory
 export PREFIX=~/arm
-export PATH=$PATH:$PREFIX/bin
-export SCRIPTDIR=`pwd`
+export PATH=$PREFIX/bin:$PATH
+export SCRIPTDIR=$(pwd)
+# 1.4 Stop on first error
+set -e
+# 1.5 Be verbose
+set -x
+# 1.6 Check if clean
+if [ -d "$PREFIX" ]; then
+        echo ""
+        echo "The following directories may be overwritten."
+        echo " - $PREFIX"
+        echo ""
+        read -r -p "Are you sure? [y/N] " response
+        case $response in
+            [yY][eE][sS]|[yY])
+                ;;
+            *)
+                exit
+                ;;
+        esac
+fi
 
 ###
 # 2. Prepare the build environment
 # 2.1 Create the directory structure
-mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/build
-mkdir -p $PREFIX/orig
-mkdir -p $PREFIX/src
-# 2.2 Download and untar the source archives
+if [ ! -d $PREFIX/bin ]; then
+	mkdir -p $PREFIX/bin
+fi
+if [ ! -d $PREFIX/build ]; then
+	mkdir -p $PREFIX/build
+fi
+if [ ! -d $PREFIX/orig ]; then
+	mkdir -p $PREFIX/orig
+fi
+if [ ! -d $PREFIX/src ]; then
+	mkdir -p $PREFIX/src
+fi
+# 2.2 Download and unpack the source archives
+# 2.2.1 Download Binutils
 cd $PREFIX/orig
 if [ ! -f $BINUTILS.tar.gz ]; then
 	wget ftp://ftp.gnu.org/gnu/binutils/$BINUTILS.tar.gz
+fi
+# 2.2.2 Unpack Binutils
+if [ ! -d $PREFIX/src/$BINUTILS ]; then
 	cd $PREFIX/src
 	tar xzf $PREFIX/orig/$BINUTILS.tar.gz
 fi
-
+# 2.2.3 Download GCC
 cd $PREFIX/orig
 if [ ! -f $GCC.tar.gz ]; then
 	wget ftp://ftp.gnu.org/gnu/gcc/$GCC/$GCC.tar.gz
+fi
+if [ ! -d $PREFIX/src/$GCC ]; then
 	cd $PREFIX/src
 	tar xzf $PREFIX/orig/$GCC.tar.gz
 fi
-
+# 2.2.4 Download Newlib
 cd $PREFIX/orig
 if [ ! -f $NEWLIB.tar.gz ]; then
 	wget ftp://sources.redhat.com/pub/newlib/$NEWLIB.tar.gz
+fi
+# 2.2.5 Unpack Newlib
+if [ ! -d $PREFIX/src/$NEWLIB ]; then
 	cd $PREFIX/src
 	tar xzf $PREFIX/orig/$NEWLIB.tar.gz
 	if [ $NEWLIB -eq "newlib-2.1.0" ]; then
@@ -87,22 +132,28 @@ if [ ! -f $NEWLIB.tar.gz ]; then
 		patch -p0 < $SCRIPTDIR/newlib-2.1.0.patch/
 	fi
 fi
-
+# 2.2.6 Download GDB
 cd $PREFIX/orig
 if [ ! -f $GDB.tar.gz ]; then
 	wget ftp://ftp.gnu.org/gnu/gdb/$GDB.tar.gz
+fi
+# 2.2.7 Unpack GDB
+if [ ! -d $PREFIX/src/$GDB ]; then
 	cd $PREFIX/src
 	tar xzf $PREFIX/orig/$GDB.tar.gz
 fi
-# 2.5 Create the build directories
+# 2.3 Create build directories
 cd $PREFIX/build
-mkdir `ls $PREFIX/src`
+for x in $(ls $PREFIX/src); do
+	if [ ! -d $x ]; then
+		mkdir $x
+	fi
+done
 
 ### 
 # 3. Build & install GNU ARM cross-toolchain
 # 3.1 Build Binutils
 cd $PREFIX/build/$BINUTILS
-make clean
 $PREFIX/src/$BINUTILS/configure --target=$TARGET --prefix=$PREFIX --with-cpu=cortex-m4 --with-fpu=fpv4-sp-d16 --with-float=hard --with-mode=thumb --enable-interwork --enable-multilib --with-gnu-as --with-gnu-ld --disable-nls
 # The meaning of flags:
 # --with-cpu=cortex-m4 ..... cortex-m4 CPU
@@ -114,12 +165,13 @@ $PREFIX/src/$BINUTILS/configure --target=$TARGET --prefix=$PREFIX --with-cpu=cor
 # --with-gnu-as ............ use only GNU Assembler
 # --with-gnu-ld ............ use only GNU Linker
 # --disable-nls ............ output only in English
+# make clean
 make -j4 all
 make install
+
 # 3.2 Build & install bootstrap GCC (C cross-compiler only)
 cd $PREFIX/build/$GCC
-make clean
-$PREFIX/src/$GCC/configure --target=$TARGET --prefix=$PREFIX --with-cpu=cortex-m4 --with-fpu=fpv4-sp-d16 --with-float=hard --with-mode=thumb --enable-interwork --enable-multilib --enable-languages="c" --with-system-zlib --with-newlib --without-headers --disable-shared --disable-nls --with-gnu-as --with-gnu-ld
+$PREFIX/src/$GCC/configure --target=$TARGET --prefix=$PREFIX --with-cpu=cortex-m4 --with-fpu=fpv4-sp-d16 --with-float=hard --with-mode=thumb --enable-interwork --enable-multilib --with-system-zlib --with-newlib --without-headers --disable-shared --disable-nls --with-gnu-as --with-gnu-ld --enable-languages="c"
 # The meaning of flags (see https://gcc.gnu.org/install/configure.html):
 # --with-system-zlib ...... use the system's zlib library
 
@@ -127,23 +179,34 @@ $PREFIX/src/$GCC/configure --target=$TARGET --prefix=$PREFIX --with-cpu=cortex-m
 # --with-newlib ........... newlib is going to be used as the target C library
 # --without-headers ....... do not use target headers from libc (newlib) when building, because newlib is not build yet.
 # --disable-shared ........ only static libraries, because shared are not supported on the target platform
+# make clean
 make -j4 all-gcc
 make install-gcc
+
 # 3.3 Build & install newlib library
 cd $PREFIX/build/$NEWLIB
-make clean
-$PREFIX/src/$NEWLIB/configure --target=$TARGET --prefix=$PREFIX --with-cpu=cortex-m4 --with-fpu=fpv4-sp-d16 --with-float=hard --with-mode=thumb --enable-interwork --enable-multilib --disable-newlib-supplied-syscalls --with-gnu-as --with-gnu-ld --disable-nls
+$PREFIX/src/$NEWLIB/configure --target=$TARGET --prefix=$PREFIX --with-cpu=cortex-m4 --with-fpu=fpv4-sp-d16 --with-float=hard --with-mode=thumb --enable-interwork --enable-multilib --disable-newlib-supplied-syscalls --with-gnu-as --with-gnu-ld --disable-nls --enable-newlib-nano-malloc
 # The meaning of flags:
 # --disable-newlib-supplied-syscalls ... disable syscalls, because we are building for bare-metal target.
-make -j4 all
-make install
-# Build & install GCC C, C++, libstdc++ with newlib library
-cd $PREFIX/build/$GCC
-$PREFIX/src/$GCC/configure --target=$TARGET --prefix=$PREFIX --with-cpu=cortex-m4 --with-fpu=fpv4-sp-d16 --with-float=hard --with-mode=thumb --enable-interwork --enable-interwork --enable-multilib --enable-languages="c,c++" --with-system-zlib --with-newlib --disable-shared --disable-nls --with-gnu-as --with-gnu-ld
-make -j4 all
-make install
-# Build & install GDB debugger
-$PREFIX/src/$GDB/configure --target=$TARGET --prefix=$PREFIX --enable-interwork --enable-multilib
+# --enable-newlib-nano-malloc ... enable nano implementation of malloc suitable for devices with limited memory resources
+# make clean
 make -j4 all
 make install
 
+# 3.4 Build & install GCC C, C++, libstdc++ with newlib library
+cd $PREFIX/build/$GCC
+$PREFIX/src/$GCC/configure --target=$TARGET --prefix=$PREFIX --with-cpu=cortex-m4 --with-fpu=fpv4-sp-d16 --with-float=hard --with-mode=thumb --enable-interwork --enable-interwork --enable-multilib --with-system-zlib --with-newlib --disable-shared --disable-nls --with-gnu-as --with-gnu-ld --enable-languages="c,c++"
+# make clean
+make -j4 all
+make install
+
+# 3.5 Build & install GDB debugger
+cd $PREFIX/build/$GDB
+$PREFIX/src/$GDB/configure --target=$TARGET --prefix=$PREFIX
+# make clean
+make -j4
+make install
+
+echo ""
+echo "### END ###"
+echo ""
