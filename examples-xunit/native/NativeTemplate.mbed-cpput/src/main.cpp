902e9a82101cc0c898d1bc9cf336cc3693526045
Iztok Starc <iztok.starc@fri.uni-lj.si>
2014-11-04 13:07:17 +0100

    Updated *-cpput projects

--- examples-xunit/native/NativeTemplate.mbed-cpput/src/main.cpp
+++ examples-xunit/native/NativeTemplate.mbed-cpput/src/main.cpp
@@ -11,6 +11,7 @@
   */
 #include "mbed.h"
 #include "add.h"
+#include "dadd.h"
 
 DigitalOut myled(LED1);
 
@@ -18,31 +19,9 @@ int main() {
     int i = 0;
     while(1) {
         i = add(i,1);
+        i = dadd(i,1);
         printf("%d Hello world!\n", i);
-        myled = i%2;
+        myled = i%3;
         wait(i%10);
     }
 }
-
-/*
- * Override C++ new/delete operators to reduce memory footprint
- */
-#ifdef CUSTOM_NEW
-
-void *operator new(size_t size) {
-        return malloc(size);
-}
-
-void *operator new[](size_t size) {
-        return malloc(size);
-}
-
-void operator delete(void *p) {
-        free(p);
-}
-
-void operator delete[](void *p) {
-        free(p);
-}
-
-#endif
