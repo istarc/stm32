52a6e6edd11c0874e536bedeced91ae936444912
Iztok Starc <iztok.starc@fri.uni-lj.si>
2014-12-14 09:26:03 +0100

    Added some minor fixes and improved comments

--- gitlab/deploy.sh
+++ gitlab/deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 ##
+# Basic Descripton
+#
 # This is a GitLab & GitLab CI deployment utility. You can now setup 
-#  private git repository management system with continuous integration.
+#  private git repository management system with continuous integration
+#  for STM32 embedded systems development on a single host.
 #
 # - GitLab server offers "git repository management, code reviews, 
 #    issue tracking, activity feeds, wikis".
@@ -11,6 +14,20 @@
 #    GitLab installation to run tests for your projects". It supports
 #    GNU Tools for ARM Embedded Processors
 #    (see https://launchpad.net/gcc-arm-embedded).
+#
+
+##
+# Some Further Details
+#
+# - This GitLab & GitLab CI system is deployed as a docker container
+#    to decrease host system pollution. These containers can be stopped,
+#    restarted or moved to a different system. Only ruby, ruby-mechanize,
+#    wget, curl, nc and lxc-docker-1.3.2 packages are installed. The latter
+#    is installed from a 3rd party repository maintained by docker.com.
+#
+# - The GitLab & GitLab CI store data on host computer to avoid data loss, 
+#    when docker containers are deleted. The GitLab & GitLab CI data store 
+#    is located at "/opt" directory.
 
 ##
 # Prerequisites: Ubuntu 14.04 LTS
@@ -24,7 +41,7 @@
 # 0.1 Stop on first error
 set -e 
 # 0.2 Be verbose
-# set -x
+set -x
 # 0.3 PostgreSQL Configuration
 # 0.3.1 GitLab DB
 export DB_NAME="gitlab"
@@ -43,7 +60,17 @@ if [ $(ip addr | grep $GITLAB_IP | wc -l) == 0 ]; then
 	exit 1
 fi
 export GITLAB_PORT="80"
+if [ $(nc -z $GITLAB_IP $GITLAB_PORT; echo $?) == 0 ]; then
+	echo "Error: GitLab HTTP port $GITLAB_PORT already in use."
+	echo "Try: change the GITLAB_PORT value."
+	exit 1
+fi
 export GITLAB_SSH_PORT="22"
+if [ $(nc -z $GITLAB_IP $GITLAB_SSH_PORT; echo $?) == 0 ]; then
+	echo "Error: GitLab SSH port $GITLAB_SSH_PORT already in use."
+	echo "Try: change the GITLAB_SSH_PORT value."
+	exit 1
+fi
 # 0.5 GitLab CI Configuration
 export GITLABCI_IP=192.168.255.3
 if [ $(ip addr | grep $GITLABCI_IP | wc -l) == 0 ]; then
@@ -54,6 +81,11 @@ if [ $(ip addr | grep $GITLABCI_IP | wc -l) == 0 ]; then
 	exit 1
 fi
 export GITLAB_CI_PORT="80"
+if [ $(nc -z $GITLABCI_IP $GITLAB_CI_PORT; echo $?) == 0 ]; then
+	echo "Error: GitLab HTTP port $GITLAB_CI_PORT already in use."
+	echo "Try: change the GITLAB_CI_PORT value."
+	exit 1
+fi
 # 0.6 GitLab CI Runner Configuration
 export CI_SERVER_URL="http://$GITLABCI_IP:$GITLAB_PORT"
 export CI_RUNNERS_COUNT=2
@@ -74,19 +106,31 @@ if [ -z "$(cat /etc/os-release | grep "Ubuntu 14.04")" ]; then
 	esac
 fi
 # 0.8 Get Existing Docker Containers ID
+#
+# DANGER ZONE
+#  Stop and remove all containers
+#   sudo docker stop $(sudo docker ps -a -q) 
+#   sudo docker rm $(sudo docker ps -a -q)
+#  Remove all untagged images
+#   sudo docker rmi $(sudo docker images | grep "^<none>" | awk '{print $3}')
+#
 export CONTAINER_IDS=$(sudo docker ps -a -q)
-export POSTGRES_ID=$(sudo docker inspect --format '{{.Name}} {{.Config.Hostname}}' $CONTAINER_IDS | grep -e '^/postgresql ' | cut -d' ' -f2)
-export GITLAB_ID=$(sudo docker inspect --format '{{.Name}} {{.Config.Hostname}}' $CONTAINER_IDS | grep -e '^/gitlab ' | cut -d' ' -f2)
-export GITLABCI_ID=$(sudo docker inspect --format '{{.Name}} {{.Config.Hostname}}' $CONTAINER_IDS | grep -e '^/gitlab-ci ' | cut -d' ' -f2)
-export GITLABCI_RUNNER_ID=$(sudo docker inspect --format '{{.Name}} {{.Config.Hostname}}' $CONTAINER_IDS | grep -e '^/gitlab-ci-runner ' | cut -d' ' -f2)
+if [ ! -z "$CONTAINER_IDS" ]; then
+	export POSTGRES_ID=$(sudo docker inspect --format '{{.Name}} {{.Config.Hostname}}' $CONTAINER_IDS | grep -e '^/postgresql ' | cut -d' ' -f2)
+	export GITLAB_ID=$(sudo docker inspect --format '{{.Name}} {{.Config.Hostname}}' $CONTAINER_IDS | grep -e '^/gitlab ' | cut -d' ' -f2)
+	export GITLABCI_ID=$(sudo docker inspect --format '{{.Name}} {{.Config.Hostname}}' $CONTAINER_IDS | grep -e '^/gitlab-ci ' | cut -d' ' -f2)
+	export GITLABCI_RUNNER_ID=$(sudo docker inspect --format '{{.Name}} {{.Config.Hostname}}' $CONTAINER_IDS | grep -e '^/gitlab-ci-runner ' | cut -d' ' -f2)
+fi
 # 0.9 Check Existing Data Store
 if [ -d "/opt/postgresql" ] || [ -d "/opt/gitlab" ] || [ -d "/opt/gitlab-ci" ] || [ -d "/opt/gitlab-ci-runner" ]; then
 	echo ""
-	echo "Delete stored data and existing docker containers?"
+	echo "Wipe out previous deployment?"
+	echo ""
 	echo " - /opt/postgresql"
 	echo " - /opt/gitlab"
 	echo " - /opt/gitlab-ci"
 	echo " - /opt/gitlab-ci-runner"
+	echo " - corresponding docker containers"
 	echo ""
 	read -r -p "Are you sure? [y/N] " response
 	echo ""
@@ -113,12 +157,12 @@ if [ ! -f "/etc/apt/sources.list.d/docker.list" ]; then
 	sudo sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
 	sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 fi
-#sudo apt-get update -q
+sudo apt-get update -q
 # 1.2 Install project dependancies
 # 1.2.1 Install Docker 1.3.2
-#sudo apt-get install -y lxc-docker-1.3.2
+sudo apt-get install -y lxc-docker-1.3.2
 # 1.2.2 Install Ruby
-#sudo apt-get install -y ruby ruby-mechanize wget curl
+sudo apt-get install -y ruby ruby-mechanize wget curl nc
 
 # 2. Deploy PostgreSQL
 echo -e "Deploying PostgreSQL"
@@ -196,7 +240,7 @@ if [ ! -d "/opt/gitlab-ci-runner" ]; then
 # 5.1A Create Data Store
 	sudo mkdir -p /opt/gitlab-ci-runner
 # 5.2A Donwload the CI registration Token Retrieval Script
-	wget https://raw.githubusercontent.com/sammcj/getcitoken/master/getcitoken.rb
+	wget -O getcitoken.rb https://raw.githubusercontent.com/sammcj/getcitoken/master/getcitoken.rb
 # 5.3A Provide Appropriate Configuration File
 	cat <<-EOF > config.yml
 	gitlab_ci_url: 'http://$GITLABCI_IP:$GITLAB_CI_PORT'
