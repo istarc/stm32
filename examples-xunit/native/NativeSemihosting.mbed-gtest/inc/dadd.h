562640ba3199d32e3defdd2612b3f611e2a16ce9
Iztok Starc <iztok.starc@fri.uni-lj.si>
2014-12-20 21:27:54 +0100

    Added two project templates with googletest support

---
+++ examples-xunit/native/NativeSemihosting.mbed-gtest/inc/dadd.h
@@ -0,0 +1,6 @@
+#ifndef __DADD_H__
+#define __DADD_H__
+
+extern "C" int dadd(int, int);
+
+#endif
---
+++ examples-xunit/sim/SimTemplate.mbed-gtest/src/main.cpp
@@ -0,0 +1,16 @@
+#include "mbed.h"
+#include "add.h"
+#include "dadd.h"
+
+DigitalOut myled(LED1);
+
+int main() {
+    int i = 0;
+    while(1) {
+        i = add(i,1);
+        i = dadd(i,1);
+        printf("%d Hello world!\n", i);
+        myled = i%3;
+        wait(i%10);
+    }
+}
